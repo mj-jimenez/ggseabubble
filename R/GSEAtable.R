c96dfbecc6644642708765a0815ffc3f1b93f958
mjjimenez <mj.jimenezsantos@outlook.com>
2021-12-29 16:35:12 +0100

    Change underscores by points in variable's names

--- R/GSEAtable.R
+++ R/GSEAtable.R
@@ -15,19 +15,19 @@ GSEAtable <- function(dir) {
   # Read all reports and add two columns with the name of the comparison and the
   # signature used.
   tables <- lapply(files, function(x) {
-    comp_dir <- basename(dirname(x))
+    comp.dir <- basename(dirname(x))
     signature <- gsub(pattern = ".Gsea.*$", replacement = "" ,
-                      tail(unlist(strsplit(comp_dir, "_")), n = 1))
-    comparison <- unlist(strsplit(comp_dir, split = paste0("_", signature)))[1]
+                      tail(unlist(strsplit(comp.dir, "_")), n = 1))
+    comparison <- unlist(strsplit(comp.dir, split = paste0("_", signature)))[1]
     t <- read.table(paste0(dir, "/", x), sep = "\t", header = TRUE) %>%
       select(-X) %>% mutate(COMPARISON = comparison, SIGNATURE = signature)
   })
   # Bind all reports, reorder the columns and convert some columns to factors
   # and other to numeric.
-  gsea_table <- do.call("rbind", tables) %>%
+  gsea.table <- do.call("rbind", tables) %>%
     relocate(NAME:LEADING.EDGE, .after = last_col()) %>%
     mutate(across(COMPARISON:GS.br..follow.link.to.MSigDB, as.factor),
            across(SIZE:RANK.AT.MAX, as.numeric)) %>%
     relocate(NAME:LEADING.EDGE, .after = last_col())
-  return(gsea_table)
+  return(gsea.table)
 }
